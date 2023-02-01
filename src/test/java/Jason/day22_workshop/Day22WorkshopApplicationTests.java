dec97d953f9dffd76bef9e1c7348c350bb896e43
Jason Lee Jian Ying <79128355+Jasonljy90@users.noreply.github.com>
2023-02-01 12:52:50 +0800

    initial commit

---
+++ src/test/java/Jason/day22_workshop/Day22WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package Jason.day22_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day22WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
